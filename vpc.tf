28aca24f8c15f94819638298d74df37cd25baf3f
PeanutButterOnionSamich <joshuat@outlook.com>
2020-04-08 12:59:12 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,13 +2,3 @@ module "vpc" {
   source  = "app.terraform.io/JoshuaThompson-Training/vpc/aws"
   version = "2.33.0"
 }
-
-resource "vpc" "hashicat_vpc" {
-  cidr_block = "10.1.0.0/16"
-  
-  tags = {
-    Name = "${var.prefix}-vpc"
-    Billable = true
-    Department = "devops"
-  }
-}
